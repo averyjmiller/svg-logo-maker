7b571ac4026b5ed6ceb4b6fc1761460963cc7c3e
Avery-lab <54604339+Avery-lab@users.noreply.github.com>
2023-07-09 17:58:08 -0400

    added inquirer questions in the lib/prompts.js file and added a lib/generateSVG.js file

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const { Square, Circle, Triangle } = require('./lib/shapes.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateSVG = require('./lib/generateSVG.js');
+const prompts = require('./lib/prompts.js');
+
+function writeToFile(fileName, data) {
+  // fs.writeFile(fileName, generateSVG(data), (err) => 
+  //   err ? console.log(err) : console.log('Generated logo.svg')
+  // );
+  console.log(fileName);
+  console.log(data);
+}
+
+function init() {
+  inquirer
+  .prompt(prompts)
+  .then((response) => {
+    writeToFile("logo.svg", response);
+  })
+  .catch((err) => {
+    if(err.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+init();
